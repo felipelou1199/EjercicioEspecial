a23b4c24093480ace4054a3fc6e49598c8088ffe
Felipe Rosas <felipe.rosas1199@gmail.com>
2025-04-30 11:53:11 -0600

    Correcion Commit No.2

--- html/js/main.js
+++ html/js/main.js
@@ -27,30 +27,30 @@ const LoadProducts = e => {
 }
 //createCards
 function createCards(prods) {
-    prods.forEach(e => {
-        const img = e.images && e.images[1] ? e.images[1] : e.images[0];
-        console.log(img);
-        
+  for(let i = 0; i<prods.length;i++){
+    const img = prods[i].images && prods[i].images[1] ? prods[i].images[1] : prods[i].images[0];
         cardsContainer.insertAdjacentHTML("beforeend",
             `
         <div class="col">
           <div class="card shadow-sm">
-          <img class="bd-placeholder-img card-img-top" src="${img}" alt="${e.tittle}" width="100%" height="225" style="object-fit: cover;">
+          <img class="bd-placeholder-img card-img-top" src="${img}" alt="${prods[i].tittle}" width="100%" height="225" style="object-fit: cover;">
             <div class="card-body">
-              <p class="card-text"> ${e.description}</p>
+              <p class="card-text"> ${prods[i].description}</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
-                <small class="text-body-secondary">$ ${e.price}</small>
+                <small class="text-body-secondary">$ ${prods[i].price}</small>
               </div>
             </div>
           </div>
         </div>
         `
         )
-    });
+        if(i>=8)break;
+
+  }
 }
 //Click
 btnLoadProductos.addEventListener("click", LoadProducts);
